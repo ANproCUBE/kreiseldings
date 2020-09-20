bdc6d0e05b3a91ad716b558c5e2c1b720e7db87c
ANproCUBE <71607868+ANproCUBE@users.noreply.github.com>
2020-09-20 21:32:15 +0200

    Update kreiselDings.cpp

--- kreiselDings.cpp
+++ kreiselDings.cpp
@@ -1,5 +1,6 @@
 //Italienische Spiralnudeln lieben diesen Trick!
 //Orig: https://pr0gramm.com/top/4140917
+//Auch noch: https://pr0gramm.com/new/3282451
 
 #define OLC_PGE_APPLICATION
 #include "engine.h"
@@ -37,16 +38,16 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		Clear(olc::WHITE); //Nach jedem Frame alten Content löschen
+		Clear(olc::WHITE); //Nach jedem Frame alten Content lÃ¶schen
 
 		for (int i = 1; i < 21; i++)
 		{
-			DrawCircle(scrW, scrH, i * 15, olc::BLACK); //20 Große Kreise zeichnen, 1. mit r=15
+			DrawCircle(scrW, scrH, i * 15, olc::BLACK); //20 GroÃŸe Kreise zeichnen, 1. mit r=15
 			elem[i-1].x = (sin(elem[i-1].winkel) * (i * 15)) + scrW;
 			elem[i-1].y = (cos(elem[i-1].winkel) * (i * 15)) + scrH;
 			FillCircle(elem[i-1].x, elem[i-1].y, 3, olc::RED); //Punkte auf den Kreisen zeichnen
 			elem[i-1].winkel = elem[i-1].winkel + 0.01 + ((double)i/10000);
-			//^ Hier den Winkel verändern, wenn die FPS zu hoch und die Drehung zu schnell ist
+			//^ Hier den Winkel verÃ¤ndern, wenn die FPS zu hoch und die Drehung zu schnell ist
 			//FPS-Unterschiede sind bei der jeweiligen Kompilierart sowie auf verschiedenen Systemen zu erwarten
 		}
 
